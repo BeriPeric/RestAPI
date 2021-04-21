27a975059fd2dbffad9ab18fb61223c90c286a83
Beri Gill <38020683+BeriPeric@users.noreply.github.com>
2021-04-21 15:31:41 -0400

    Create RestServiceProject4Application.java

---
+++ RestServiceProject4Application.java
@@ -0,0 +1,13 @@
+package com.example.restserviceProject4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceProject4Application.class, args);
+	}
+
+}
